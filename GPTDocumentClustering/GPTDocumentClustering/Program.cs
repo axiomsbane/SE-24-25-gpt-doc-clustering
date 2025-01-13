80cb2de8279f96d0ec3af571e3c0a8f7139eeff0
Aditya Shidhaye <axiom@Adityas-MacBook-Air.local>
2025-01-13 17:01:48 +0100

    Committed gitignore and code change

--- GPTDocumentClustering/GPTDocumentClustering/Program.cs
+++ GPTDocumentClustering/GPTDocumentClustering/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace GPTDocumentClustering;
+using OpenAI.Chat;
 
 class Program
 {
     static void Main(string[] args)
     {
-        string key = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
-        Console.WriteLine(key);
+        ChatClient client = new(model: "gpt-4o", apiKey: Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+
+        
+        int numMessages = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < numMessages; i++)
+        {
+            ChatCompletion completion = client.CompleteChat(Console.ReadLine());
+            Console.WriteLine($"[ASSISTANT]: {completion.Content[0].Text}");
+        }
+        
     }
 }
