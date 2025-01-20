63dcc76e78cd0355c211188b2c4911b61b8425db
Aditya Shidhaye <ashidhaye2@gmail.com>
2025-01-20 17:05:41 +0100

    Embedding API call is working, confirmed that

--- GPTDocumentClustering/GPTDocumentClustering/Program.cs
+++ GPTDocumentClustering/GPTDocumentClustering/Program.cs
@@ -1,6 +1,8 @@
-﻿using GPTDocumentClustering.Interfaces.InputData;
+﻿using System.Text.RegularExpressions;
+using GPTDocumentClustering.Interfaces.InputData;
 using GPTDocumentClustering.Models;
 using GPTDocumentClustering.Services.InputData;
+using OpenAI.Embeddings;
 
 namespace GPTDocumentClustering;
 using OpenAI.Chat;
@@ -22,9 +24,21 @@ class Program
         
         List<Document> documents = service.ReadDocuments();
         Console.WriteLine("Document Count: " + documents.Count);
+        foreach (Document document in documents)
+        {
+            document.Content = Regex.Replace(document.Content.Trim(), @"\r\n?|\n", " ");
+        }
+        
+        EmbeddingClient client = new("text-embedding-3-small", Environment.GetEnvironmentVariable("OPENAI_API_KEY"));
+        EmbeddingGenerationOptions options = new() { Dimensions = 15 };
+        
+        
         for (int i = 0; i < 3; i++)
         {
-            Console.WriteLine(documents[i].Content.Trim());
+            OpenAIEmbedding embedding = client.GenerateEmbedding(documents[i].Content, options);
+            ReadOnlyMemory<float> vector = embedding.ToFloats();
+            Console.WriteLine(string.Join(", ", vector.ToArray().Select(x => x.ToString("F4"))));
+            //Console.WriteLine(Regex.Replace(documents[i].Content.Trim(), @"\r\n?|\n", " "));
             Console.WriteLine("#############################\n#########################\n######################");
             Console.WriteLine(documents[i].Category);
             Console.WriteLine("#############################\n#########################\n######################");
