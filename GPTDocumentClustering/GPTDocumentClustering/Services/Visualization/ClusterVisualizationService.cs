d3320728b9e2e0407688ce1e020addfbb74d4e62
Pradeep Patwa <pradeeppatwa72@gmail.com>
2025-02-09 20:44:47 +0530

    updated code

--- GPTDocumentClustering/GPTDocumentClustering/Services/Visualization/ClusterVisualizationService.cs
+++ GPTDocumentClustering/GPTDocumentClustering/Services/Visualization/ClusterVisualizationService.cs
@@ -11,16 +11,16 @@ public class ClusterVisualizationService
         //var vectors = clusteredDocuments.Select(x => Vector<double>.Build.DenseOfArray(x.Document.Embedding)).ToArray();
         //var clusters = clusteredDocuments.Select(x => x.ClusterIndex).ToList();
 
-        // Apply PCA - to reduced embedding in 2 Dimension
-        var pca = new PrincipalComponentAnalysis();
+        //// Apply PCA - to reduced embedding in 2 Dimension
+        //var pca = new PrincipalComponentAnalysis();
         //var reducedData = pca.Reduce(vectors, 2);
 
-        // Output coordinates
-        Console.WriteLine("--- Reduced Document Coordinates ---");
+        //// Output coordinates
+        //Console.WriteLine("--- Reduced Document Coordinates ---");
         //for (int i = 0; i < reducedData.RowCount; i++)
-        {
-         //   Console.WriteLine($"Document {i}, Cluster {clusters[i]}: X = {reducedData[i, 0]:F4}, Y = {reducedData[i, 1]:F4}");
-        }
-        Console.WriteLine("--- End Reduced Document Coordinates ---");
+        //{
+        //    Console.WriteLine($"Document {i}, Cluster {clusters[i]}: X = {reducedData[i, 0]:F4}, Y = {reducedData[i, 1]:F4}");
+        //}
+        //Console.WriteLine("--- End Reduced Document Coordinates ---");
     }
 }
