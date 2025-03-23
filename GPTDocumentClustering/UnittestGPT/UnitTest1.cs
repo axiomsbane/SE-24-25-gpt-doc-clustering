83898329743efb5bfa668b9721b1e26088b08aa7
Moltereas <adithya20501@gmail.com>
2025-03-23 12:08:21 +0100

    wrote some unit test cases for the similarity and clustering services as they are updated

--- GPTDocumentClustering/UnittestGPT/UnitTest1.cs
+++ GPTDocumentClustering/UnittestGPT/UnitTest1.cs
@@ -1,77 +1,34 @@
-﻿using Moq;
-using NUnit.Framework;
-using NUnit.Framework.Legacy;
-using GPTDocumentClustering.Services.InputData;
+﻿using System.Collections.Generic;
+using System.Linq;
 using GPTDocumentClustering.Models;
-using GPTDocumentClustering.Services.Embedding;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System;
-using GPTDocumentClustering.Interfaces.InputData;
-using Microsoft.VisualStudio.TestPlatform.TestHost;
+using GPTDocumentClustering.Services.Clustering;
+using NUnit.Framework;
 
 namespace GPTDocumentClustering.Tests
 {
-    public class ProgramTests
+    [TestFixture]
+    public class ClusteringServiceTests
     {
-        // Test for CsvDataReader to ensure it returns documents correctly
         [Test]
-        public void CsvDataReader_ReadDocuments_ReturnsDocuments()
+        public void ClusterEmbeddings_ShouldAssignClusters_ToDocuments()
         {
             // Arrange
-            var mockCsvReader = new Mock<IReadInputData>();
-            var expectedDocuments = new List<Document>
-            {
-                new Document { Content = "Document 1", Category = "Category 1" },
-                new Document { Content = "Document 2", Category = "Category 2" }
-            };
-            mockCsvReader.Setup(x => x.ReadDocuments()).Returns(expectedDocuments);
-
-            // Act
-            var actualDocuments = mockCsvReader.Object.ReadDocuments();
-
-            // Assert
-            ClassicAssert.AreEqual(expectedDocuments.Count, actualDocuments.Count);
-            ClassicAssert.AreEqual(expectedDocuments[0].Content, actualDocuments[0].Content);
-            ClassicAssert.AreEqual(expectedDocuments[1].Category, actualDocuments[1].Category);
-        }
-
-        // Test for EmbeddingService to ensure embeddings are generated correctly
-        [Test]
-        public async Task EmbeddingService_GenerateEmbeddings_ReturnsEmbeddings()
-        {
-            // Arrange
-            var mockEmbeddingService = new Mock<EmbeddingService>();
             var documents = new List<Document>
             {
-                new Document { Content = "Document 1" },
-                new Document { Content = "Document 2" }
+                new Document { SerialNo = 1, Embedding = new double[] { 1.0, 2.0 } },
+                new Document { SerialNo = 2, Embedding = new double[] { 1.1, 2.1 } },
+                new Document { SerialNo = 3, Embedding = new double[] { 5.0, 6.0 } },
+                new Document { SerialNo = 4, Embedding = new double[] { 5.1, 6.1 } }
             };
 
-            var expectedEmbeddings = new List<float[]> { new float[] { 0.1f, 0.2f }, new float[] { 0.3f, 0.4f } };
-            mockEmbeddingService.Setup(x => x.GenerateEmbeddings(It.IsAny<List<Document>>()))
-                .ReturnsAsync(expectedEmbeddings);
+            var clusteringService = new ClusteringService();
 
             // Act
-            var actualEmbeddings = await mockEmbeddingService.Object.GenerateEmbeddings(documents);
+            clusteringService.ClusterEmbeddings(documents);
 
             // Assert
-            ClassicAssert.AreEqual(expectedEmbeddings.Count, actualEmbeddings.Count);
-            ClassicAssert.AreEqual(expectedEmbeddings[0], actualEmbeddings[0]);
-        }
-
-        // Test for exception handling when an exception is thrown
-        [Test]
-        public void Main_ThrowsException_HandlesError()
-        {
-            // Arrange
-            var mockCsvReader = new Mock<IReadInputData>();
-            var mockEmbeddingService = new Mock<EmbeddingService>();
-            mockCsvReader.Setup(x => x.ReadDocuments()).Throws(new Exception("Data read error"));
-
-            // Act & Assert
-            var ex = Assert.ThrowsAsync<Exception>(async () => await Program.Main(new string[] { }));
-            ClassicAssert.AreEqual("An error occurred: Data read error", ex.Message);
+            Assert.That(documents.All(d => d.ClusterId != null), Is.True);
+            Assert.That(documents.Select(d => d.ClusterId).Distinct().Count(), Is.EqualTo(4));
         }
     }
 }
--- GPTDocumentClustering/UnittestGPT/cossimtest.cs
+++ GPTDocumentClustering/UnittestGPT/cossimtest.cs
@@ -154,3 +154,52 @@ namespace GPTDocumentClustering.Tests
         }
     }
 }
+
+//using System.Collections.Generic;
+//using GPTDocumentClustering.Models;
+//using GPTDocumentClustering.Services.Validation;
+//using NUnit.Framework;
+
+//namespace GPTDocumentClustering.Tests
+//{
+//    [TestFixture]
+//    public class CosineSimilarityServiceTests
+//    {
+//        [Test]
+//        public void EvaluateClusterQuality_ShouldReturnValidMetrics()
+//        {
+//            // Arrange
+//            var documents = new List<Document>
+//            {
+//                new Document { SerialNo = 1, ClusterId = 0, Embedding = new double[] { 1.0, 0.0 } },
+//                new Document { SerialNo = 2, ClusterId = 0, Embedding = new double[] { 0.9, 0.1 } },
+//                new Document { SerialNo = 3, ClusterId = 1, Embedding = new double[] { 0.0, 1.0 } }
+//            };
+
+//            var service = new CosineSimilarityService(documents);
+
+//            // Act
+//            var metrics = service.EvaluateClusterQuality();
+
+//            // Assert
+//            Assert.That(metrics, Is.Not.Null);
+//            Assert.That(metrics.AverageIntraClusterSimilarity, Is.InRange(0, 1));
+//            Assert.That(metrics.AverageInterClusterSimilarity, Is.InRange(0, 1));
+//        }
+
+//        [Test]
+//        public void CosineSimilarity_ShouldReturnExpectedValue()
+//        {
+//            // Arrange
+//            var service = new CosineSimilarityService(new List<Document>());
+//            double[] vec1 = { 1, 0 };
+//            double[] vec2 = { 0, 1 };
+
+//            // Act
+//            double result = service.CosineSimilarity(vec1, vec2);
+
+//            // Assert
+//            Assert.That(result, Is.EqualTo(0).Within(1e-5));
+//        }
+//    }
+//}
