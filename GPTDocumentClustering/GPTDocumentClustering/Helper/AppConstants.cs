50487980dbe5e156b5dc7819e3892f3fb43c8a49
Aditya Shidhaye <ashidhaye2@gmail.com>
2025-03-23 08:45:07 +0100

    Some changes in the output labelling logic

--- GPTDocumentClustering/GPTDocumentClustering/Helper/AppConstants.cs
+++ GPTDocumentClustering/GPTDocumentClustering/Helper/AppConstants.cs
@@ -6,17 +6,27 @@ public static class AppConstants
     public static class OpenAI
     {
         public static string ApiKey { get; } = LoadEnvKey("OPENAI_API_KEY");
-        public static int EmbeddingLength { get; } = 512;
+        public static int EmbeddingLength { get; } = 3072;
     }
 
     public static class DataConstants
     {
         public static string EmbeddingFilePath { get; } = LoadEnvKey("JSON_WRITE_PATH");
         public static string InputDataFilePath { get; } = LoadEnvKey("INPUT_FILE_PATH");
+        
+        public static Dictionary<string, string> MyDictionary = new Dictionary<string, string>
+        {
+            { "0", "Politics" },
+            { "1", "Sport" },
+            { "2", "Technology" },
+            { "3", "Entertainment" }
+        };
     }
 
     private static string LoadEnvKey(string envKey)
     {
         return Environment.GetEnvironmentVariable(envKey) ?? string.Empty;
     }
+    
+    
 }
