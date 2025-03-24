8338a92345ead2fc9439316dbb122138dfef8f18
Aditya Shidhaye <ashidhaye2@gmail.com>
2025-03-24 12:00:22 +0100

    commenting changes

--- GPTDocumentClustering/GPTDocumentClustering/Services/Embedding/EmbeddingGenerator.cs
+++ GPTDocumentClustering/GPTDocumentClustering/Services/Embedding/EmbeddingGenerator.cs
@@ -2,12 +2,20 @@ using GPTDocumentClustering.Helper;
 using OpenAI.Embeddings;
 
 namespace GPTDocumentClustering.Services.Embedding;
-
+/// <summary>
+/// This class implements the simple functionality of communicating with OpenAPI
+/// and returning the embedding vector for the provided string text
+/// </summary>
 public class EmbeddingGenerator
 {
     private EmbeddingClient _client = new("text-embedding-3-large", AppConstants.OpenAI.ApiKey);
     private EmbeddingGenerationOptions _options = new() { Dimensions = AppConstants.OpenAI.EmbeddingLength };
-
+    
+    /// <summary>
+    /// Returns embedding array of type double[]
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
     public Task<double[]>  GenerateEmbeddings(string text)
     {
         OpenAIEmbedding embedding = _client.GenerateEmbedding(text, _options);
--- GPTDocumentClustering/GPTDocumentClustering/Services/InputData/MyCsvMap.cs
+++ GPTDocumentClustering/GPTDocumentClustering/Services/InputData/MyCsvMap.cs
@@ -3,6 +3,10 @@ using GPTDocumentClustering.Models;
 
 namespace GPTDocumentClustering.Services.InputData;
 
+/// <summary>
+/// This class is needed for defining the mapping
+/// from the CSV column name to the parameter in Document class
+/// </summary>
 internal sealed class MyCsvMap : ClassMap<Document>
 {
     public MyCsvMap()
