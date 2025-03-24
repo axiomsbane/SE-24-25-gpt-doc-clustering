19f80ac59ab941fe34a74ef47140f1d42de020a5
Moltereas <adithya20501@gmail.com>
2025-03-24 18:19:55 +0100

    wrote some unit testcases for the embed generator and clustor visualization modules

---
+++ GPTDocumentClustering/UnittestGPT/Class2.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using GPTDocumentClustering.Models;
+using GPTDocumentClustering.Services.Visualization;
+using NUnit.Framework;
+
+namespace GPTDocumentClustering.Tests
+{
+    [TestFixture]
+    public class ClusterVisualizationServiceTests
+    {
+        [Test]
+        public void ApplyPCA_ShouldReduceDimensions()
+        {
+            // Arrange
+            var documents = new List<Document>
+            {
+                new Document { SerialNo = 1, Embedding = new double[] { 1.0, 2.0, 3.0 } },
+                new Document { SerialNo = 2, Embedding = new double[] { 4.0, 5.0, 6.0 } }
+            };
+
+            var service = new ClusterVisualizationService(documents);
+
+            // Act
+            var reducedPoints = service.ApplyPCA();
+
+            // Assert
+            Assert.That(reducedPoints.Count, Is.EqualTo(2));
+            Assert.That(reducedPoints.All(p => p.Item1 is double), Is.True);
+            Assert.That(reducedPoints.All(p => p.Item2 is double), Is.True);
+        }
+    }
+}
